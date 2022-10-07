038edc455d17cbebe24806187e5b3e754664de38
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-06 18:36:23 -0700

    Puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ * file: 4-puts.c
+ * Auth: Romero Arango Victor F <5670@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzle
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
