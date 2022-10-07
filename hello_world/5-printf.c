d53144e7fd136e21f042837be47bb4f14881b9ae
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-06 18:52:58 -0700

    Printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,16 +1,19 @@
 /*
- * File: 5-printf.c 
- * Auth: Romero Arango Victor F 
+ * File: 5-printf.c
+ * Auth: Romero Arango Victor F
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is a piece of art."
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
