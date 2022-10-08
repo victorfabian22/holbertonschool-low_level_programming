4a1e51fed89411728eaeef716e1d50f6fb64c37c
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-08 14:50:39 -0700

    Print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 3-print_alphabets.c
+ * Auth: Romero Arango Victor
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+	
+	for (num = 0; num < 10; num++)
+	       printf("%d" , num);
+
+printf("\");
+
+return (0);
+}
