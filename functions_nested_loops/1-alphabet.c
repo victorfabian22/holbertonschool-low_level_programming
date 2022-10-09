acedb56cc5bc9abc0f9a388dbc0714bcf5b4bbdd
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-09 12:58:55 -0700

    Alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_alphabet - print letters of the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+#include "main.h"
+/**
+ * print_alphabet - print letters of the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}}
