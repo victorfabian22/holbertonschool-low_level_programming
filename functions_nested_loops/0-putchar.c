8be2b82569e478b18cadd0a2bd80bcab560b574a
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-09 12:14:30 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ * File 0-putchar.c
+ * Auth: Romero Arango Victor
+ *
+ */
+
+#include "_putchar"
+
+/**
+ * main - Write a program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+ int main(void)
+ {
+	 _putchar('_');
+	 _putchar('p');
+	 _putchar('u');
+	 _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+	 _putchar('\n');
+
+	 return(0);
+	}
