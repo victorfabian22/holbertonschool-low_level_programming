a53057f8d25610c99053eef6e804d3067832d20e
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-09 13:12:57 -0700

    absolute value of an integer.

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - print a number
+ * @a: the number to be checked
+ *
+ * Description: Print absolute number of a
+ * Return: a positive number
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (-a);
+	}
+
+		return (a);
+}
