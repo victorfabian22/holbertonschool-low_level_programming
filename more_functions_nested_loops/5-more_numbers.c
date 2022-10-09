6e8f7ce3abc5a9ffb46a7d243b2e849f67ef4b58
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-09 13:54:43 -0700

    More numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers
+ *
+ * Description: Print numbers from 0 to 14 followed by a newline
+ * Return: Always return 0
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a > 9)
+				_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
