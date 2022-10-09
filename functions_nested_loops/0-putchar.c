d89b9d749e613a11d87b2ac8c1f19992236de7e3
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-09 12:53:33 -0700

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,22 +1,28 @@
-#include "main.h"
-
-/**
- * main - Write a program that prints _putchar, followed by a new line.
+/*
+ * File: 0-holberton.c
+ * Auth: Brennan D Baraban
  *
- * 
- * Return: Always return 0
  */
 
+#include "holberton.h"
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
