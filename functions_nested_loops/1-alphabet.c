73e1b26ba9a3ca9f91ddd183e2aa63b77fddd8a0
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-09 12:35:48 -0700

    Alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,23 +1,15 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * print_alphabet - print chars
- *
- * Return: Always return 0
+ * print_alphabet - prints alphabet in lowercase
+ * Return: void
  */
-
 void print_alphabet(void)
 {
-	int a;
+	int n;
 
-	a = 97;
-
-	while (a <= 122)
-	{
-
-		_putchar(a);
-
-		a++;
-	}
-	_putchar('\n');
+	for (n = 97; n <= 122; n++)
+		_putchar(n);
+	_putchar(10);
 }
