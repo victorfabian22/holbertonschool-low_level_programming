648f3024c9fdd98f765b64f731e1ad2ef1f3601d
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-14 15:30:48 -0700

    Reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_998 - update the value of a pinted variable to 98
+ * @n:int variable
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+        *n = 98;
+}	
