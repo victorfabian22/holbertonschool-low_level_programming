3d8f0b10978e80bdb8b2a1af80a0d3d1f078a012
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-14 15:54:44 -0700

    Puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * puts2 - prints every other character of a string (odd)
+ * @str: string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int size;
+	int i;
+
+	size = 0;
+	while (*(str + size) != '\0')
+		size++;
+	for (i = 0; i <= size && *(str + i) != '\0'; i = i + 2)
+		_putchar(str[i]);
+	_putchar(10);
+}
