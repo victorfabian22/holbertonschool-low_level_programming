b726e78a439ce644d623c4f7ee609b40abecb05e
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-14 15:31:37 -0700

    main

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void _puts(char *str);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
