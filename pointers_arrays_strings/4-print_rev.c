d6aa750300bdf0b100cf6588a2284ce1f4cd480a
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-14 15:52:06 -0700

    Print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = *s;
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		i = *s++;
+		count++;
+	}
+	if (count != 0)
+	{
+		i = *s--;
+		while (count > 0)
+		{
+			i = *s--;
+			_putchar(i);
+			count--;
+		}
+	}
+	_putchar(10);
+}
