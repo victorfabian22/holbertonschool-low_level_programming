7269b431c37e5e7ad454012eb0e987fbbad1326c
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-15 13:43:49 -0700

    Print diagsmus

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -3,7 +3,6 @@
 
 /**
  * print_diagsums - Write a function that prints the sum of the two diagonals of a square matrix of integers.
- * of a square matrix of integers
  * @a: square matrix of which we print the sum of diagonals
  * @size: size of the matrix
  */
