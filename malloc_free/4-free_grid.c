92d553c415bda4311eda872c1936b83737f40855
victorfabian22 <fabianromeroa22@gmail.com>
2022-10-26 19:50:34 -0700

    Free Grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * *free_grid - Write a function that frees a 2 dimensional grid
+ * *free_grid -  free a 2 dimensional grid
  * @grid: **int
  *@height: int
  */
