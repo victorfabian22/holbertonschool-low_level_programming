140be51bc47da5928076e19dbe815f9bf566589b
victorfabian22 <fabianromeroa22@gmail.com>
2022-11-05 18:18:19 -0700

    Free dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,18 +1,18 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
- * free_dog - frees heap memory from dog vars
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
  *
- * @d: dog record to free
- *
- * Return: new dog record;
+ * Return: void
  */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-	free(d->owner);
-	free(d->name);
-	free(d);
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
 }
