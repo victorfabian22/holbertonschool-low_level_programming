27e33e84b9c574517074765b95d988e4ab0271d2
victorfabian22 <fabianromeroa22@gmail.com>
2022-11-16 16:11:57 -0800

    Add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,88 @@
+#include "lists.h"
+
+/**
+ * _strLengthPlus - Function that returns the length of a string.
+ * @s:              Pointer to a string.
+ * Return:          A variable that contains the length of the string.
+ */
+int _strLengthPlus(const char *s)
+{
+	int b = 0;
+
+	do {
+		b++;
+	} while (*s++);
+
+	return (b);
+}
+
+/**
+ * *_strcpyPlus - Function that copies a string included the null bite
+ *                to a buffer.
+ * @dest:         Pointer to a buffer.
+ * @src:          Pointer to string.
+ * Return:        Pointer to dest.
+ */
+char *_strcpyPlus(char *dest, const char *src)
+{
+	int b = 0, len;
+
+	len = _strLengthPlus(src);
+	while (b < len)
+	{
+		*dest++ = *src++;
+		b++;
+	}
+	return (dest - b);
+}
+/**
+ * *_strdup - Function returns a pointer to a new string which is
+ *            a duplicate of the string s.
+ * @s:        Pointer to a string.
+ * Return:    A pointer to a new duplicated string.
+ */
+
+char *_strdup(const char *s)
+{
+	char *p;
+	int len;
+
+	if (!s)
+		return (NULL);
+
+	len = _strLengthPlus(s);
+
+	p = malloc(sizeof(char) * len);
+
+	if (!p)
+		return (NULL);
+
+	_strcpyPlus(p, s);
+
+	return (p);
+}
+
+/**
+ * add_node - Function that adds a node at the beginning of a list.
+ * @head:     Double pointer to linked list.
+ * @str:      Pointer to a string.
+ * Return:    Address of new element.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int length;
+	list_t *newNode;
+
+	newNode = malloc(sizeof(list_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->str = _strdup(str);
+	length = _strLengthPlus(str);
+
+	newNode->len = length - 1;
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
