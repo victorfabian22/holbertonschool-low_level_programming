0900a9ce222992bfae24b0069865bae4f1df8044
victorfabian22 <fabianromeroa22@gmail.com>
2022-11-16 16:05:23 -0800

    List lenght

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- *list_len -  function that returns the number of elements in a linked list_t list.
+ *list_len -   returns the number of elements in a linked list_t list.
  *@h: Pointer to list_t
  *Return: The number of elements of linked list_t list
  */
