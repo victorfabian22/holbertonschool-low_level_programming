7dc67904d393f23d430a0b76a9d4d8bcca3f09c1
victorfabian22 <fabianromeroa22@gmail.com>
2022-11-19 14:33:19 -0800

    Add nodeint

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - function that adds a new node at the beginning of a listint_t list.
+ * add_nodeint - function  adds a new node at the beginning of a listint_t list.
  *               of a listint_t list.
  * @head:        A pointer to the address of the
  *               head of the listint_t list.
