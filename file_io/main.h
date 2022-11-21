5fc12ff6d9ed65ac35de786d68c895ed1427dbbf
victorfabian22 <fabianromeroa22@gmail.com>
2022-11-21 15:41:15 -0800

    Main

--- file_io/main.h
+++ file_io/main.h
@@ -1,16 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <unistd.h>
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 #endif
-Footer
-© 2022 GitHub, Inc.
