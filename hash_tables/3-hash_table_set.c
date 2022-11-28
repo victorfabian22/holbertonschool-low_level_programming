baed8ac33773103d7166cda9305fbe2489ca215c
victorfabian22 <fabianromeroa22@gmail.com>
2022-11-28 13:56:54 -0800

    Hash table set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,53 +1,51 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_set - Function that adds an element to the hash table.
- * @ht:             Hash table that we want to add or update.
- * @key:            A key, can't be an empty string.
- * @value:          Value.
- * Return:          1 if succeeded otherwise returns 0.
+ * hash_table_set - function that adds an element to hash table
+ * @ht: hash table to update
+ * @key: key
+ * @value: value of key
+ * Return: updated table
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newNode = NULL, *temp = NULL;
-	unsigned long int index = 0;
+	unsigned long int idxn = 0;
+	char *t_val = NULL;
+	hash_node_t *temp = NULL;
+	hash_node_t *new = NULL;
 
-	if ((ht == NULL) | (key == NULL) | (value == NULL))
+	if (ht == NULL || ht->array == NULL || value == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index])
+	if (strlen(key) == 0 || key == NULL)
+		return (0);
+	t_val = strdup(value);
+	if (t_val == NULL)
+		return (0);
+	idxn = key_index((unsigned char *)key, ht->size);
+	/* Collision checker */
+	temp = ht->array[idxn];
+	while (temp)
 	{
-		temp = ht->array[index];
-		while (temp)
-		{
-			if (strcmp(temp->key, (char *)key) == 0)
+		if (strcmp(temp->key, key) == 0)
 		{
 			free(temp->value);
-			temp->value = (char *)strdup(value);
+			temp->value = t_val;
+			temp->value = strdup(value);
+			free(t_val);
 			return (1);
 		}
 		temp = temp->next;
-		}
 	}
-	newNode = malloc(sizeof(hash_node_t));
-	if (newNode == NULL)
-		return (0);
-	newNode->key = (char *)strdup(key);
-	if (newNode->key == NULL)
+	/* If no collision, insert node */
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		free(newNode);
+		free(new);
 		return (0);
 	}
-	newNode->value = (char *)strdup(value);
-	if (newNode->value == NULL)
-	{
-		free(newNode->key);
-		free(newNode);
-		return (0);
-	}
-	newNode->next = ht->array[index];
-	ht->array[index] = newNode;
+	new->key = strdup(key);
+	new->value = t_val;
+	new->next = ht->array[idxn];
+	ht->array[idxn] = new;
 	return (1);
 }
