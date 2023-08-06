d3c3719f0582e7539a667cbf788464acb9295867
= <=>
2023-08-06 13:47:37 -0700

    Search

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*__Prototypes__*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /*SEARCH_H*/
